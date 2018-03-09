f8351c6c232ce937798db865cfdeba8670ef66e5
Celine Patag <celine.patag@gmail.com>
2018-03-09 11:29:30 +0800

    webhook.resend is an instance method

--- samples/notifications/resend_webhook_event.rb
+++ samples/notifications/resend_webhook_event.rb
@@ -5,7 +5,8 @@ include PayPal::SDK::REST
 include PayPal::SDK::Core::Logging
 
 begin
-    @webhook_event = WebhookEvent.resend("WH-7Y7254563A4550640-11V2185806837105M")
+    @webhook_event = WebhookEvent.get 'WH-7Y7254563A4550640-11V2185806837105M'
+    @webhook_event.resend
     logger.info "Resent Webhook Event[#{@webhook_event.id}]"
 
 rescue ResourceNotFound => err
